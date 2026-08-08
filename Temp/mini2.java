1c631f8ac411b515d01215183033322001d2c584
Hgikn <140434556+Hgikn@users.noreply.github.com>
2026-08-09 00:18:22 +0600

    .gitignore pause

---
+++ Temp/mini2.java
@@ -0,0 +1,10 @@
+package demo;
+import java.util.*;
+public class Foo {
+  private String name;
+  public Foo(String n) { this.name = n; }
+  public static void main(String[] args) {
+    System.out.println("hi");
+  }
+}
+
